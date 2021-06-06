bd5e157ef72c2c2fa8ae917cc6bbebfdb3cdef09
Ytalo Paulo <ytalo.ypw@gmail.com>
2021-06-06 17:56:20 -0300

    Rotacionando as Cartas
    fazendo a rotação das cartas segundo tutorial do youtube.

--- js/script.js
+++ js/script.js
@@ -7,6 +7,15 @@
             var card = document.querySelector("#card" + i);
             card.style.left = i % 3 === 0 ? 5 + "px" : i % 3 * 305 + 5 + "px";
             card.style.top = i < 3 ? 5 + "px" : 310 + "px";
+
+            card.addEventListener("click", flipCard, false);
         }
+    }
+
+    function flipCard(){
+        var faces = this.getElementsByClassName("face");
+        faces[0].classList.toggle("flipped");
+        faces[1].classList.toggle("flipped");
+        
     }
 }());
