3940dda8268b23397e735c47e1a3669926d6adfd
Ytalo Paulo <ytalo.ypw@gmail.com>
2021-06-06 18:21:49 -0300

    Construindo as Cartas
    Distribuições das imagens.

--- js/script.js
+++ js/script.js
@@ -1,14 +1,31 @@
 (function(){
     
+    var imagens = [];
+
+    for(var i = 0; i < 6; i++){
+        var img = {
+            src: "img/" + i + ".jpeg",
+            id: i % 3
+        };
+        imagens.push(img);
+    }
+
     starGame();
 
     function starGame(){
+
+        var frontFaces = document.getElementsByClassName("front");
+
         for(var i = 0; i < 6; i++){
             var card = document.querySelector("#card" + i);
             card.style.left = i % 3 === 0 ? 5 + "px" : i % 3 * 305 + 5 + "px";
             card.style.top = i < 3 ? 5 + "px" : 310 + "px";
 
             card.addEventListener("click", flipCard, false);
+
+            frontFaces[i].style.background = "url('"+ imagens[i].src +"')";
+            frontFaces[i].setAttribute("id", imagens[i].id);
+            console.log(frontFaces[i].id);
         }
     }
 
